53d7c135be83e9e3900e70831ce7a8f9a4ea3eae
Chan Mo <opopopqkr@nate.com>
2017-12-08 18:02:31 +0900

    hello logback

--- src/main/java/kr/ac/hansung/controller/HomeController.java
+++ src/main/java/kr/ac/hansung/controller/HomeController.java
@@ -4,6 +4,8 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -17,8 +19,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller // @Component + 컨트롤러 역할
 public class HomeController {
 	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	//private static final Logger logger = LoggerFactory.getLogger("kr.ac.hansung.controller.HomeController");
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String showHome(Locale locale, Model model) {
+	public String showHome(HttpServletRequest request, Locale locale, Model model) {
+		
+		logger.info("trace level: Welcome home! The client locale is {}", locale);
+		
+		String url = request.getRequestURL().toString();
+		String clientIPaddress = request.getRemoteAddr();
+		
+		logger.info("Request URL: " + url);
+		logger.info("ClientIP: " + clientIPaddress);
 		
 		return "home";
 	}
